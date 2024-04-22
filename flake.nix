14b03bb83de19501c61916600752935f58b1fc2c
Philip Munksgaard <philip@munksgaard.me>
2024-04-22 21:57:04 +0200

    Add nix flake for rambo binary

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          craneLib = inputs.crane.lib.${system};
+
+          rambo_bin = craneLib.buildPackage { src = ./priv; };
+        in {
+          packages.rambo_bin = rambo_bin;
+          packages.default = self'.packages.rambo_bin;
+        };
+      flake = { };
+    };
+}
